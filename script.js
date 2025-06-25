7dc002856097d8e59d95d5da752722233ae51306
zooomy321 <aungminnthant33@gmail.com>
2025-06-25 18:06:12 +0800

    Version 0.2.0
    Added multiplication, buttons to toggle between addition, multiplication and both, simplified code, changed from version 0.1.2 to 0.2.0

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-let input = document.getElementById("input");
-let equation = document.getElementById("equation");
-let solved = document.getElementById("solved");
-
-let new_problem = function() {
-    let x = Math.floor(Math.random() * 10) + 1;
-    let y = Math.floor(Math.random() * 10) + 1;
+const input = document.getElementById("input");
+const equation = document.getElementById("equation");
+const solved = document.getElementById("solved");
+const addition_button = document.getElementById("addition");
+const multiplication_button = document.getElementById("multiplication");
+const both_button = document.getElementById("both");
+let ans
+let x
+let y
+let question_type = 0
+function addition() {
+    x = Math.ceil(Math.random() * 100);
+    y = Math.ceil(Math.random() * 100);
     equation.innerText = x + " + " + y + " = "
     ans = x+y
     return ans
 }
-new_problem();
+function multiplication() {
+    x = Math.ceil(Math.random() * 10);
+    y = Math.ceil(Math.random() * 10);
+    equation.innerText = x + " × " + y + " = "
+    ans = x*y
+    return ans
+}
+function new_problem() {
+    if (question_type === 0) {
+        addition()
+    } else if (question_type === 1) {
+        multiplication()
+    } else if (question_type === 2) {
+        if (Math.random() > 0.5) {
+            addition()
+        } else {
+            multiplication()
+        }
+    }
+}
+addition_button.addEventListener("click", () => question_type = 0)
+multiplication_button.addEventListener("click", () => question_type = 1)
+both_button.addEventListener("click", () => question_type = 2)
+
+new_problem()
 input.addEventListener("input", () => {
     if (Number(input.value) === ans) {
         input.value = null
