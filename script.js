7b65c822cd1a5418a31e882f22c880fa94fd0051
zooomy321 <aungminnthant33@gmail.com>
2025-06-27 08:52:47 +0800

    Version 0.2.2
    - Start/Stop button functionality added, with 3 second countdown before practice starts
    - Added Timing and Average Time Per Question Stats
    - Rewrote some code

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ const multiplication_button = document.getElementById("multiplication");
 const both_button = document.getElementById("both");
 const start_button = document.getElementById("Start");
 const stop_button = document.getElementById("Stop");
+const time = document.getElementById("time");
+const cannot_change_qns_type_msg = document.getElementById("cannot_change_qns_type_msg");
+const average = document.getElementById("average");
 stop_button.style.display = 'none'
 let buttons = [addition_button,multiplication_button,both_button]
 let ans
 let x
 let y
-let question_type = 0
+let stop_value = false
+let question_type = -1
+let start_pressed = false
 function addition() {
     x = Math.ceil(Math.random() * 100);
     y = Math.ceil(Math.random() * 100);
@@ -41,6 +46,7 @@ function new_problem() {
 }
 buttons.forEach(element => {
     element.addEventListener("click", () => {
+        if (stop_button.style.display == 'none') {
         question_type = buttons.indexOf(element)
         buttons.forEach(button => {
             if (button === element) {
@@ -48,23 +54,64 @@ buttons.forEach(element => {
             } else {
                 button.style.backgroundColor = "#271828"
             }
-        })
+        })} else {
+            buttons_pressed_during_practice();
+        }
     })
 });
+function buttons_pressed_during_practice() {
+    cannot_change_qns_type_msg.innerText = "Cannot change during practice!";
+    setTimeout(() => {cannot_change_qns_type_msg.innerText = ""},3000);
+}    
+function time_run() {
+  const interval = setInterval(() => {
+    if (stop_value == true) {
+        clearInterval(interval);
+        return;
+    } else {
+        time.innerText = (Number(time.innerText) + 0.01).toFixed(2);
+    }
+  }, 10);
+}
+const average_time = () => (Number(time.innerText) / Number(solved.innerText)).toFixed(2);
+
 start_button.addEventListener("click", () => {
+    start_pressed = true;
+    if (question_type == -1) {
+        equation.innerText = "Choose Question Type First!";
+        return;
+    }
+    time.innerText = 0.00;
+    average.innerText = 0;
+    solved.innerText = 0;
+    stop_value = 0;
     start_button.style.display = 'none';
     stop_button.style.display = 'inline';
+    setTimeout(() => {equation.innerText = "Starting in 3..."}, 0);
+    setTimeout(() => {equation.innerText = "Starting in 2..."}, 1000);
+    setTimeout(() => {equation.innerText = "Starting in 1..."}, 2000);
+    setTimeout(() => {
+        input.style.display = 'inline';
+        new_problem();
+        time_run();
+        start_pressed = false;}, 3000);
 })
+    
 stop_button.addEventListener("click", () => {
-    start_button.style.display = 'inline';
-    stop_button.style.display = 'none';
+    if (start_pressed == true) {return}
+    else {
+        stop_value = true;
+        start_button.style.display = 'inline';
+        stop_button.style.display = 'none';
+        input.style.display = 'none';
+        equation.innerText = "Press Start!";
+    }
 })
-
-new_problem()
 input.addEventListener("input", () => {
     if (Number(input.value) === ans) {
         input.value = null
         solved.innerText = Number(solved.innerText) + 1
         new_problem()
+        average.innerText = average_time()
     }
 })
